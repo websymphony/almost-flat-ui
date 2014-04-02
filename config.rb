26d5c00b0bcc66c65bf48598a7efbaf76fb87dae
Amit Gaur <amitgaur.web@gmail.com>
2014-04-01 21:32:25 -0500

    upgraded to foundation 5.2.1

--- config.rb
+++ config.rb
@@ -1,17 +1,17 @@
 require 'compass'
-require 'zurb-foundation'
 # Require any additional compass plugins here.
-
+add_import_path "bower_components/foundation/scss"
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "stylesheets"
-sass_dir = "sass"
+sass_dir = "scss"
 images_dir = "images"
 javascripts_dir = "javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
   output_style = :expanded
+  # output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
